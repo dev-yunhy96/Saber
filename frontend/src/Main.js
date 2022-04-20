584a9f52d046d12b88d241582ec4c34cfca152f8
권도혁 <fsa3806@naver.com>
2022-04-20 15:52:59 +0900

    initial commit

---
+++ frontend/src/Main.js
@@ -0,0 +1,13 @@
+import App from "./components/App";
+import CourseListPage from "./pages/CourseListPage";
+import HomePage from "./pages/HomePage";
+
+function Main() {
+  return (
+    <App>
+      <CourseListPage />
+    </App>
+  );
+}
+
+export default Main;
