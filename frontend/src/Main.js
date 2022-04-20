f35c263d91ff6c6badbf5fcaf8ddf5d06c80f9e7
권도혁 <fsa3806@naver.com>
2022-04-20 16:03:28 +0900

    fix:initial homepage

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,11 +1,10 @@
 import App from "./components/App";
-import CourseListPage from "./pages/CourseListPage";
 import HomePage from "./pages/HomePage";
 
 function Main() {
   return (
     <App>
-      <CourseListPage />
+      <HomePage />
     </App>
   );
 }
