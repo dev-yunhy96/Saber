99d9c9c881e9bb82a3d5beef54e274de14fbd4d5
DESKTOP-3L721M6\USER <wjdtjq1121@gmail.com>
2022-04-26 14:09:47 +0900

    first commit
    Former-commit-id: 8fb4c98322b04e0c8e2c30d9992068c9dc46cf97

---
+++ backend/src/test/java/com/fourteam/saber/SaberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourteam.saber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
