052468ad1010af552d7d8aafcc22bd3014ce030e
권도혁 <fsa3806@naver.com>
2022-04-26 11:27:58 +0900

    feat: login

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage() {
-  return <div>로그인페이지</div>;
+  const [Email, setEamil] = useState("");
+  const [Password, setPassword] = useState("");
+  const onEmailHandler = (event) => {
+    setEamil(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    let body = {
+      email: Email,
+      password: Password,
+    };
+  };
+  return (
+    <div>
+      <div>로그인페이지</div>
+      <form onSubmit={onSubmitHandler}>
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
