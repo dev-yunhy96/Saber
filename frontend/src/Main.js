3a8a7f36908aea99eedb9d835a1653390e8b8f56
yunhy96 <yunhy96@naver.com>
2022-04-26 10:51:39 +0900

    to pull

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,11 +1,16 @@
 import App from "./components/App";
 import CourseListPage from "./pages/CourseListPage";
+import CoursePage from "./pages/CoursePage";
 import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import QuestionListPage from "./pages/QuestionListPage";
+import QuestionPage from "./pages/QuestionPage";
+import WishlistPage from "./pages/WishlistPage";
 
 function Main() {
   return (
     <App>
-      <CourseListPage />
+      <HomePage />
     </App>
   );
 }
