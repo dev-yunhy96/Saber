71b026863bd4ce9e79c859e7420f2ed451eeb7fe
권도혁 <fsa3806@naver.com>
2022-05-04 10:21:56 +0900

    feature: redux login nav
    Former-commit-id: 222b4c42155e89b989751a2edf08dae9f79af513

--- frontend/src/components/UserMenu.js
+++ frontend/src/components/UserMenu.js
@@ -2,13 +2,25 @@ import { useCallback, useEffect, useState } from "react";
 import personIcon from "../assets/person.png";
 import styles from "./UserMenu.module.css";
 import { useNavigate } from "react-router-dom";
+import { store } from "../features/store";
 function UserMenu() {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
-
+  const [login, setLogin] = useState(store.getState().user.user);
+  const [check, setCheck] = useState(false);
+  useEffect(() => {
+    if (Object.keys(login).length !== 0 && check === false) {
+      setCheck(true);
+      return;
+    } else if (Object.keys(login).length === 0 && check === true) {
+      setCheck(false);
+      return;
+    }
+  }, [login, check]);
   const handleButtonClick = useCallback((e) => {
     e.stopPropagation();
     setIsOpen((nextIsOpen) => !nextIsOpen);
+    setLogin(store.getState().user.user);
   }, []);
 
   useEffect(() => {
@@ -30,8 +42,12 @@ function UserMenu() {
       {isOpen && (
         <ul className={styles.popup}>
           <li onClick={() => navigate("/wishlist")}>위시리스트</li>
-          <li onClick={() => navigate("/signup")}>회원가입</li>
-          <li onClick={() => navigate("/login")}>로그인</li>
+          {!check && (
+            <>
+              <li onClick={() => navigate("/signup")}>회원가입</li>
+              <li onClick={() => navigate("/login")}>로그인</li>
+            </>
+          )}
         </ul>
       )}
     </div>
