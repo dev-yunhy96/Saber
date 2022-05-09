a8db0e720896bb52c1018c1d9dba5eb6ab2a1977
sahngrhee <87463462+sahngrhee@users.noreply.github.com>
2022-05-09 10:17:51 +0900

    fix error
    Former-commit-id: cc945c363d54418f9e752203226cb0c29a204c38

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -9,8 +9,6 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SamplePage from "./pages/SamplePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import MyPageDemo2 from "./pages/MyPageDemo2";
 import MyPage from "./pages/MyPage";
 
 function Main() {
@@ -32,7 +30,6 @@ function Main() {
           <Route path="mypage" element={<MyPage />} />
           <Route path="sample" element={<SamplePage />} />
           <Route path="*" element={<NotFoundPage />} />
-          <Route path="mypage2" element={<MyPageDemo2 />} />
         </Route>
       </Routes>
     </BrowserRouter>
