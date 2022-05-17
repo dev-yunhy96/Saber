c966a00db8bb6f2b0698938ab75d08e94ac9a468
권도혁 <fsa3806@naver.com>
2022-05-17 10:47:09 +0900

    feat: comment update design and send battle
    Former-commit-id: 958296e92a6403c8ff901ef94256b7b60663889b [formerly 5c4167bbc678d6f5bdc8838e3a58dda5f0681c41]
    Former-commit-id: bfa65e67c70c4a0b210db80a555d346b6ae4358c

--- frontend/src/common/api/serverApi.js
+++ frontend/src/common/api/serverApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const baseUrl = "https://k6a404.p.ssafy.io/api/v1";
+// const baseURL = "https://k6a404.p.ssafy.io/api/v1";
 
 export default axios.create({
   baseURL: "http://localhost:8080/api/v1",
