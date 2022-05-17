055717de7433249457078d4e36b445f18b2d7c7b
sahngrhee <87463462+sahngrhee@users.noreply.github.com>
2022-05-17 14:08:50 +0900

    logout
    Former-commit-id: 6931d84e6ac5c8165c19e5cfdc2d9ca66fd237a0 [formerly fd30e3227a71fdf0a8125850d108127b31cdbe9d]
    Former-commit-id: 24b845a2ec4b8301341438d676e514272aef6b2c

--- frontend/src/components/NaverLoginCallBack.js
+++ frontend/src/components/NaverLoginCallBack.js
@@ -1,20 +1,25 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import serverApi from "../common/api/serverApi";
+import { useDispatch } from "react-redux";
+import { fetchAsyncUserDetail } from "../features/user/userSlice";
 
 const NaverLoginCallBack = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const accessToken = location.hash.split("=")[1].split("&")[0];
-  console.log(accessToken);
 
   const getUserInfo = () => {
     if (location.hash) {
       serverApi
-        .get(`/oauth/naver?accessToken=${accessToken}`, {
-          // withCredentials: true,
+        .get(`/oauth/naver?accessToken=${accessToken}`, {})
+        .then((response) => {
+          localStorage.setItem("token", response.data.token);
+          dispatch(fetchAsyncUserDetail(response.data.token));
         })
-        .then((res) => {
-          console.log(res);
+        .then(() => {
+          navigate("/");
         })
         .catch((error) => {
           console.error(error);
